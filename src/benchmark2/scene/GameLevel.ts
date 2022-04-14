278360f89331b86827efad7d98814e6b1f0f3a2d
Andrew Ojeda <andrew.ojeda@stonybrook.edu>
2022-04-14 13:54:46 -0400

    abstracted out the loading of resources that will be present in all rooms/levels

--- src/benchmark2/scene/GameLevel.ts
+++ src/benchmark2/scene/GameLevel.ts
@@ -41,6 +41,28 @@ export default abstract class GameLevel extends Scene {
     this.MAX_PROJECTILE_SIZE
   );
 
+  //this will be loaded in every single level, allows level classes to be abstracted out
+  loadMainResources() {
+    this.load.spritesheet("player1", "res/spritesheets/mcbendorjee.json");
+    this.load.spritesheet("slice", "res/spritesheets/slice.json");
+    this.load.spritesheet("flag", "res/spritesheets/flag.json");
+    this.load.spritesheet("blueRobot", "res/spritesheets/r_blue.json");
+    this.load.spritesheet("blueMouse", "res/spritesheets/rm_blue.json");
+    this.load.spritesheet("blueStatue", "res/spritesheets/rs_blue.json");
+    this.load.spritesheet("projectile", "res/spritesheets/projectile.json");
+    this.load.spritesheet("bomb", "res/spritesheets/explode.json");
+    this.load.spritesheet("greenFlag", "res/spritesheets/green_flag.json");
+    this.load.object("weaponData", "res/data/weaponData.json"); // Load scene info
+    this.load.object("navmesh", "res/data/navmesh.json"); // Load nav mesh
+    this.load.image("healthpack", "res/sprites/healthpack.png");
+    this.load.image("inventorySlot", "res/sprites/inventory.png");
+    this.load.image("knife", "res/sprites/knife.png");
+    this.load.image("laserGun", "res/sprites/laserGun.png");
+    this.load.image("pistol", "res/sprites/pistol.png");
+    this.load.image("block", "res/sprites/block.png");
+    this.load.audio("boom", "res/sound/explode.wav");
+  }
+
   initScene(options: Record<string, any>): void {
     options.currentScore
       ? (this.currentScore = options.currentScore)
