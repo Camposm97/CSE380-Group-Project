1e0ee482a6969db18bc514570b4dc28bd00cf98e
Michael Campos <camposm1205@gmail.com>
2022-04-25 18:10:38 -0400

    Finished editing Level 4

--- src/benchmark2/scene/Level4.ts
+++ src/benchmark2/scene/Level4.ts
@@ -4,16 +4,10 @@ import { Level5_1 } from "./Level5";
 export class Level4_1 extends GameLevel {
     loadScene(): void {
         this.loadMainResources()
-        let bombJSONArray = [
-        "res/data/level1/bombs1_1.json",
-        "res/data/level1/bombs1_1var2.json",
-        ];
-
-        this.loadRandomBombsJSON(bombJSONArray);
-
         this.load.tilemap("level", "res/tilemaps/level4/Level4_1.json"); // Load tile map
         this.load.object("start_end", "res/data/level4/start_end_4_1.json"); //Load player and green flag coordinates
         this.load.object("enemyData", "res/data/level4/enemy4_1.json"); // Load enemy info
+        this.load.object("bombData", "res/data/level4/bombs4_1.json"); // Load bomb info
         this.load.object("blockData", "res/data/level4/blocks4_1.json"); // Load block info
     }
 
