981720d6f171147064f3158303dfb0a328247f1c
Andrew Ojeda <andrew.ojeda@stonybrook.edu>
2022-04-25 23:42:00 -0400

    level 2 tilemaps added, no entites loaded other than start and endpos, music added to level 2 and 6

---
+++ src/benchmark2/scene/Level2.ts
@@ -0,0 +1,114 @@
+import { GameEventType } from "../../Wolfie2D/Events/GameEventType";
+import GameLevel from "./GameLevel";
+import { Level3_1 } from "./Level3";
+
+export class Level2_1 extends GameLevel {
+  loadScene(): void {
+    // Load resources (tilemap, audio, spritesheets)
+    this.loadMainResources();
+
+    // let bombJSONArray = [
+    //   "res/data/level2/bombs2_1.json",
+    //   "res/data/level2/bombs2_1var2.json",
+    // ];
+
+    // this.loadRandomBombsJSON(bombJSONArray);
+
+    this.load.object("bombData", "res/data/level2/bombs2_1.json"); // Load bomb info
+    this.load.tilemap("level", "res/tilemaps/level2/Level2_1.json"); // Load tile map
+    this.load.object("start_end", "res/data/level2/start_end2_1.json"); //Load player and green flag coordinates
+    this.load.object("enemyData", "res/data/level2/enemy2_1.json"); // Load enemy info
+    this.load.object("blockData", "res/data/level2/blocks2_1.json"); // Load block info
+    this.load.audio("levelMusic", "res/music/CSE_380_Level_2.mp3"); // Load Music info
+  }
+
+  startScene(): void {
+    this.setName("2-1");
+    super.startScene();
+    this.setNextLevel(Level2_2);
+    this.setCurrentRoom(Level2_1); // We need this in order for the room to be reset back to itself
+  }
+
+  updateScene(deltaT: number): void {
+    super.updateScene(deltaT);
+  }
+}
+
+export class Level2_2 extends GameLevel {
+  loadScene(): void {
+    // Load resources (tilemap, audio, spritesheets)
+    this.loadMainResources();
+
+    this.load.tilemap("level", "res/tilemaps/level2/Level2_2.json"); // Load tile map
+    this.load.object("start_end", "res/data/level2/start_end2_2.json"); //Load player and green flag coordinates
+    this.load.object("enemyData", "res/data/level2/enemy2_2.json"); // Load enemy info
+    this.load.object("bombData", "res/data/level2/bombs2_2.json"); // Load bomb info
+    this.load.object("blockData", "res/data/level2/blocks2_2.json"); // Load block info
+    this.load.audio("levelMusic", "res/music/CSE_380_Level_2.mp3"); // Load Music info
+  }
+
+  startScene(): void {
+    this.setPlayMusic(false); //so music doesn't loop
+    this.setName("2-2");
+    super.startScene();
+    this.setNextLevel(Level2_3);
+    this.setCurrentRoom(Level2_2);
+  }
+
+  updateScene(deltaT: number): void {
+    super.updateScene(deltaT);
+  }
+}
+
+export class Level2_3 extends GameLevel {
+  loadScene(): void {
+    this.loadMainResources();
+    // Load resources (tilemap, audio, spritesheets)
+
+    this.load.tilemap("level", "res/tilemaps/level2/Level2_3.json"); // Load tile map
+    this.load.object("start_end", "res/data/level2/start_end2_3.json"); //Load player and green flag coordinates
+    this.load.object("enemyData", "res/data/level2/enemy2_3.json"); // Load enemy info
+    this.load.object("bombData", "res/data/level2/bombs2_3.json"); // Load bomb info
+    this.load.object("blockData", "res/data/level2/blocks2_3.json"); // Load block info
+    this.load.audio("levelMusic", "res/music/CSE_380_Level_2.mp3"); // Load Music info
+  }
+
+  startScene(): void {
+    this.setPlayMusic(false); //so music doesn't loop
+    this.setName("1-3");
+    super.startScene();
+    this.setNextLevel(Level2_4);
+    this.setCurrentRoom(Level2_3);
+  }
+
+  updateScene(deltaT: number): void {
+    super.updateScene(deltaT);
+  }
+}
+
+export class Level2_4 extends GameLevel {
+  loadScene(): void {
+    // Load resources (tilemap, audio, spritesheets)
+    this.loadMainResources();
+
+    this.load.tilemap("level", "res/tilemaps/level2/Level2_4.json"); // Load tile map
+    this.load.object("start_end", "res/data/level2/start_end2_4.json"); //Load player and green flag coordinates
+    this.load.object("enemyData", "res/data/level2/enemy2_4.json"); // Load enemy info
+    this.load.object("bombData", "res/data/level2/bombs2_4.json"); // Load bomb info
+    this.load.object("blockData", "res/data/level2/blocks2_4.json"); // Load block info
+    this.load.audio("levelMusic", "res/music/CSE_380_Level_2.mp3"); // Load Music info
+  }
+
+  startScene(): void {
+    this.setLastLevel(true);
+    this.setPlayMusic(false);
+    this.setName("1-4");
+    super.startScene();
+    super.setNextLevel(Level3_1); // TODO - Go to Level3_1
+    this.setCurrentRoom(Level2_4);
+  }
+
+  updateScene(deltaT: number): void {
+    super.updateScene(deltaT);
+  }
+}
