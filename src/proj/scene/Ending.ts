b2eec13d1fe692285df4fd97716b85e7aa6b3734
Michael Campos <camposm1205@gmail.com>
2022-05-04 19:11:49 -0400

    Updated Ending.ts

--- src/proj/scene/Ending.ts
+++ src/proj/scene/Ending.ts
@@ -1,4 +1,12 @@
+import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
+import AnimatedSprite from "../../Wolfie2D/Nodes/Sprites/AnimatedSprite";
+import Sprite from "../../Wolfie2D/Nodes/Sprites/Sprite";
 import Scene from "../../Wolfie2D/Scene/Scene";
+import { PlayerAnimations } from "./Constants";
+
+enum Layer {
+    Main = 'Main'
+}
 
 /**
  * This is where we'll display the ending scene of where 
@@ -7,12 +15,25 @@ import Scene from "../../Wolfie2D/Scene/Scene";
  * He then realizes he's late to lecture and scurries to his class.
  */
 export class Ending extends Scene {
+    private prof: AnimatedSprite
+    private pc: Sprite
+    private c: Vec2
+
     loadScene(): void {
         this.load.spritesheet('mcbendorjee', 'res/spritesheets/mcbendorjee.json')
         this.load.spritesheet('rm_blue', 'res/spritesheets/robots/robot_mouse_blue.json')
+        this.load.image('pc', 'res/sprites/computer.png')
     }
     startScene(): void {
-        
+        this.c = this.viewport.getCenter().clone()
+        this.addUILayer(Layer.Main)
+        this.pc = this.add.sprite('pc', Layer.Main)
+        this.pc.position = this.c.clone()
+        this.pc.scale = new Vec2(8,8)
+
+        this.prof = this.add.animatedSprite('mcbendorjee', Layer.Main)
+        this.prof.position = this.c.clone().sub(new Vec2(0,200))
+        this.prof.animation.play(PlayerAnimations.HAPPY, true)
     }
     updateScene(deltaT: number): void {
         
