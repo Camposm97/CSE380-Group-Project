ce8290a1084b5de1710e15dcf11a10d9d75c96cf
Andrew Ojeda <andrew.ojeda@stonybrook.edu>
2022-05-06 18:35:20 -0400

    level1_2 and level1_3 tileset updated and bomb data randomized

--- src/proj/scene/Level1.ts
+++ src/proj/scene/Level1.ts
@@ -71,10 +71,18 @@ export class Level1_3 extends GameLevel {
     this.loadMainResources();
     // Load resources (tilemap, audio, spritesheets)
 
-    this.load.tilemap("level", "res/tilemaps/level1/Level1_3.json"); // Load tile map
+    let bombJSONArray = [
+      "res/data/level1/bombs1_3.json",
+      "res/data/level1/bombs1_3var2.json",
+      "res/data/level1/bombs1_3var3.json",
+    ];
+
+    this.loadRandomBombsJSON(bombJSONArray);
+
+    this.load.tilemap("level", "res/tilemaps/level1/Level1_3_alt.json"); // Load tile map
     this.load.object("start_end", "res/data/level1/start_end1_3.json"); //Load player and green flag coordinates
     this.load.object("enemyData", "res/data/level1/enemy1_3.json"); // Load enemy info
-    this.load.object("bombData", "res/data/level1/bombs1_3.json"); // Load bomb info
+    // this.load.object("bombData", "res/data/level1/bombs1_3.json"); // Load bomb info
     this.load.object("blockData", "res/data/level1/blocks1_3.json"); // Load block info
     this.load.audio("levelMusic", "res/music/CSE_380_Level_1.mp3"); // Load Music info
   }
